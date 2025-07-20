Corrected information leakage feature which dominated and prohibited tuning

src/preprocess_rnn.py
@@ -92,22 +92,22 @@ def preprocess_RNN(DATA_DIR: Path, regions: list, seq_len: int = 24):
                      'HR',
                      'day_of_year',
                      'day_of_week',
-                     'lag_interchange_1h',
-                     'lag_interchange_24h',
-                     'interchange_roll_mean_3h',
-                     'Net_generation_lag_1',
-                     'Demand_30d_avg',
+                     # 'lag_interchange_1h',
+                     # 'lag_interchange_24h',
+                     # 'interchange_roll_mean_3h',
+                     # 'Net_generation_lag_1',
+                     # 'Demand_30d_avg',
                      'Day-ahead demand forecast_30d_avg',
-                     'Solar_30d_avg',
-                     'Wind_30d_avg',
+                     # 'Solar_30d_avg',
+                     # 'Wind_30d_avg',
                      'Net generation_30d_avg',
                      'Total interchange_30d_avg',
                      'Pct_Solar_30d_avg',
                      'Pct_Wind_30d_avg',
                      'ALLSKY_SFC_SW_DWN_30d_avg',
                      'T2M_30d_avg',
-                     'WSC_30d_avg',
-                     'unexpect_dem_diff_30d_avg'
+                     'WSC_30d_avg'# ,
+                     # 'unexpect_dem_diff_30d_avg'
                      ]
         target_col = ['Total interchange']
         df_train = df_train.drop(drop_cols,axis=1)
@@ -122,8 +122,9 @@ def preprocess_RNN(DATA_DIR: Path, regions: list, seq_len: int = 24):
                                                                                 df_test,
                                                                                 target_col)
         print(f"shape of df_train: {df_train.shape}, df_train_scaled: {df_train_scaled.shape}")
-        feature_cols = [col for col in df_train_scaled.columns if col != target_col]
+        feature_cols = [col for col in df_train_scaled.columns if col not in target_col]
         print(f"feature cols: {feature_cols} \n and target_col: {target_col}")
+
         X_train, y_train = build_sequences(df_train_scaled, feature_cols, target_col, seq_len)
         X_val, y_val = build_sequences(df_val_scaled, feature_cols, target_col, seq_len)
         X_test, y_test = build_sequences(df_test_scaled, feature_cols, target_col, seq_len)
@@ -155,6 +156,7 @@ def preprocess_RNN(DATA_DIR: Path, regions: list, seq_len: int = 24):
                     np.save(out_path, split_df)
                     print(f"Saved: {out_path.name}")
 
+
     train_end = time.time()
 
     print(f"Total time for all selected regions: {train_end - train_start} seconds")
@@ -179,9 +181,9 @@ def main():
     print(f"Figures Directory: {FIG_DIR}")
 
     # 13 EIA region codes
-    regions = ['MIDW', 'SE', 'NE', 'MIDA', 'NW', 'CENT', 'SW', 'CAR', 'CAL', 'FLA', 'NY', 'TEN', 'TEX']
+    # regions = ['MIDW', 'SE', 'NE', 'MIDA', 'NW', 'CENT', 'SW', 'CAR', 'CAL', 'FLA', 'NY', 'TEN', 'TEX']
 
-    # regions = ['MIDW']
+    regions = ['MIDW', 'NW', 'NY']
 
     preprocess_RNN(DATA_DIR, regions, seq_len=24)
 

src/regional_LSTM_GRU.py
@@ -15,7 +15,7 @@ from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from tensorflow.keras import regularizers
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, GRU, Dense, Dropout
+from tensorflow.keras import layers
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
 from keras.optimizers import Adam, RMSprop
 import keras_tuner as kt
@@ -35,7 +35,7 @@ def train_evaluate_rnn(DATA_DIR, FIG_DIR, X_train, y_train, X_val, y_val, X_test
 
     # Build model with variable type and unit structure
     model = Sequential()
-    RNN = LSTM if rnn_type.upper() == 'LSTM' else GRU
+    RNN = layers.LSTM if rnn_type.upper() == 'LSTM' else layers.GRU
 
     for i, units in enumerate(units_per_layer):
         return_seq = i < len(units_per_layer) - 1  # only last RNN layer should not return sequences
@@ -48,10 +48,10 @@ def train_evaluate_rnn(DATA_DIR, FIG_DIR, X_train, y_train, X_val, y_val, X_test
             model.add(RNN(units,
                           return_sequences=return_seq,
                           kernel_regularizer=regularizers.l2(l2_lambda)))
-        model.add(BatchNormalization())
-        model.add(Dropout(dropout))
+        model.add(layers.BatchNormalization())
+        model.add(layers.Dropout(dropout))
 
-    model.add(Dense(1))
+    model.add(layers.Dense(1))
     model.compile(optimizer=optimizer, loss=loss)
 
     # Train model using validation data, early stopping and reduce learning rate on plateau
@@ -119,7 +119,7 @@ def train_evaluate_latent_forecaster(DATA_DIR, FIG_DIR, X_train, y_train, X_val,
         tf.keras.layers.Dense(128, activation='relu',
                               kernel_regularizer=regularizers.l2(l2_lambda)),
         tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.Dropout(0.3),
+        tf.keras.layers.Dropout(0.2),
 
         tf.keras.layers.Dense(64, activation='relu',
                               kernel_regularizer=regularizers.l2(l2_lambda)),
@@ -273,17 +273,17 @@ def main():
     print(f"Figures Directory: {FIG_DIR}")
 
     # 13 EIA region codes
-    regions = ['MIDW', 'SE', 'NE', 'MIDA', 'NW', 'CENT', 'SW', 'CAR', 'CAL', 'FLA', 'NY', 'TEN', 'TEX']
-    regions = ['MIDW', 'NW', 'NY']
+    regions = ['SE', 'NE', 'MIDA', 'CENT', 'SW', 'CAR', 'CAL', 'FLA', 'TEN', 'TEX']
+    # regions = ['MIDW', 'NW', 'NY']
 
     # Run both models for comparison
-    '''print("Running Base LSTM Model")
+    print("Running Base LSTM Model")
     run_regional_rnn(DATA_DIR, FIG_DIR, regions, use_autoencoder=False,
-                            rnn_type='LSTM', units_per_layer=[128, 64, 32], epochs=30)'''
-
+                            rnn_type='LSTM', units_per_layer=[128, 64, 32], epochs=30, l2_lambda=0.0001)
+    '''
     print("Running Autoencoder Forecast Model")
-    run_regional_rnn(DATA_DIR, FIG_DIR, regions, use_autoencoder=True, epochs=30, l2_lambda=0.001)
-
+    run_regional_rnn(DATA_DIR, FIG_DIR, regions, use_autoencoder=True, epochs=30, l2_lambda=0.0001)
+    '''
 
 if __name__ == "__main__":
     main()

src/train_autoencoder.py
@@ -7,25 +7,35 @@ from tensorflow.keras import regularizers
 import tensorflow as tf
 from tensorflow.keras import layers, models, callbacks
 
-def build_autoencoder(input_shape, latent_dim=64, dropout_rate=0.2, l2_lambda=0.0):
+def build_autoencoder(input_shape, latent_dim=128, dropout_rate=0.2, l2_lambda=0.0):
     """Returns autoencoder and encoder models with dropout and batch normalization"""
     inputs = layers.Input(shape=input_shape)
 
-    # Encoder
+    # Encoder stacked layers
+    x = layers.LSTM(128, return_sequences=True,
+                    kernel_regularizer=regularizers.l2(l2_lambda))(inputs)
+    x = layers.BatchNormalization()(x)
     x = layers.LSTM(64, return_sequences=False,
                     kernel_regularizer=regularizers.l2(l2_lambda))(inputs)
     x = layers.BatchNormalization()(x)
     x = layers.Dropout(dropout_rate)(x)
+
+    # Latent layer
     latent = layers.Dense(latent_dim, activation='relu',
                           kernel_regularizer=regularizers.l2(l2_lambda))(x)
     latent = layers.BatchNormalization()(latent)
 
-    # Decoder
+    # Decoder unstacking layers
     x = layers.RepeatVector(input_shape[0])(latent)
     x = layers.LSTM(64, return_sequences=True,
                     kernel_regularizer=regularizers.l2(l2_lambda))(x)
     x = layers.BatchNormalization()(x)
     x = layers.Dropout(dropout_rate)(x)
+    x = layers.LSTM(128, return_sequences=True,
+                    kernel_regularizer=regularizers.l2(l2_lambda))(x)
+    x = layers.BatchNormalization()(x)
+
+    # Output layer
     outputs = layers.TimeDistributed(layers.Dense(input_shape[-1],
                                      kernel_regularizer=regularizers.l2(l2_lambda)))(x)
 
@@ -33,7 +43,7 @@ def build_autoencoder(input_shape, latent_dim=64, dropout_rate=0.2, l2_lambda=0.
     encoder = models.Model(inputs, latent)
     return autoencoder, encoder
 
-def train_autoencoder(DATA_DIR: Path, region: str, latent_dim=64, dropout_rate=0.2, l2_lambda=0.0):
+def train_autoencoder(DATA_DIR: Path, region: str, latent_dim=128, dropout_rate=0.2, l2_lambda=0.0):
     train_start = time.time()
     print(f"\nTraining autoencoder for region: {region}")
     X_train = np.load(DATA_DIR / f"rnn_data_X_train_{region}.npy")
@@ -67,6 +77,7 @@ def train_autoencoder(DATA_DIR: Path, region: str, latent_dim=64, dropout_rate=0
     print(f"Total time for training region: {train_end - train_start} seconds")
     return encoder
 
+
 def main():
     # Set seeds for reproducibility
     np.random.seed(42)
@@ -91,7 +102,11 @@ def main():
     regions = ['MIDW', 'NW', 'NY']
 
     for region in regions:
-        train_autoencoder(DATA_DIR, region, l2_lambda=0.001)
+        train_autoencoder(DATA_DIR, region,
+                          latent_dim=256,
+                          dropout_rate=0.2,
+                          l2_lambda=0.001
+                          )
 
 if __name__ == "__main__":
     main()